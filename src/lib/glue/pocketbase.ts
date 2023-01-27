f1824d3fd1d469123acce3d603566e2ca16c8e35
Jay Joo <jj534@cornell.edu>
2023-01-27 09:19:44 -0500

    integrate backend

--- src/lib/glue/pocketbase.ts
+++ src/lib/glue/pocketbase.ts
@@ -2,7 +2,7 @@ import PocketBase from 'pocketbase';
 
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('pocketbase url');
+export const pb = new PocketBase('https://glue2-yt-sub-only.fly.dev/');
 export const currentUser = writable(pb.authStore.model);
 
 pb.authStore.onChange((auth) => {
