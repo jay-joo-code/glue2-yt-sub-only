4ba39c6efbe46b1012596abd0842107e0d57e418
Jay Joo <jj534@cornell.edu>
2023-01-29 05:53:17 -0500

    disable auto cancelation

--- src/lib/glue/pocketbase.ts
+++ src/lib/glue/pocketbase.ts
@@ -5,6 +5,7 @@ import { writable } from 'svelte/store';
 export const pb = new PocketBase('https://glue2-yt-sub-only.fly.dev/');
 export const currentUser = writable(pb.authStore.model);
 
+pb.autoCancellation(false);
 pb.authStore.onChange((auth) => {
 	currentUser.set(pb.authStore.model);
 });
